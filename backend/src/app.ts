989cc3b32c4cbd27f27abdd39ec7a4590a24ea17
SHARIK0M <sharik.x.af@gmail.com>
2025-08-22 10:14:11 +0530

    Second-commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+
 app.use(
   (
     err: any,
@@ -48,6 +49,12 @@ app.use(
     });
   }
 );
+
+
+app.get("/", (req, res) => {
+  res.send("API is running ✅");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
